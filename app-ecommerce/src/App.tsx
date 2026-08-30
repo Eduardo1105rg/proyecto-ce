841db02725fb7669ecbf4aa216809643e323d235
LuisK19 <luis.trejos10@gmail.com>
2026-08-30 01:00:57 -0600

    feat: Refactor App component to include Navbar and ButtonTest with theme management

--- app-ecommerce/src/App.tsx
+++ app-ecommerce/src/App.tsx
@@ -1,23 +1,20 @@
-// import { useState } from 'react'
-
-// Ejemplo de import de la libreria React Router Dom
-import { HashRouter, Routes } from "react-router-dom";
-//Route
+import { HashRouter, Routes } from 'react-router-dom'
+import { Navbar } from './components/Navbar/Navbar'
+import { useTheme } from './hooks/useTheme'
+import { ButtonTest } from './pages/ButtonTest'
 import './App.css'
 
 function App() {
-
+  const { theme, toggleTheme } = useTheme()
 
   return (
-    <>
-      <h1>Pagina principal</h1>
-      <HashRouter>
-        <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-        </Routes>
-
-      </HashRouter>
-    </>
+    <HashRouter>
+      <Navbar theme={theme} onToggleTheme={toggleTheme} />
+      <ButtonTest />
+      <Routes>
+        {/* rutas aquí */}
+      </Routes>
+    </HashRouter>
   )
 }
 
