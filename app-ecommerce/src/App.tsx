ac492b73f13e99ddfd9e5ee4a57d323bed8f8944
LuisK19 <luis.trejos10@gmail.com>
2026-09-09 17:30:14 -0600

    feat: remove test routes for ButtonTest and ProductTest from App component

--- app-ecommerce/src/App.tsx
+++ app-ecommerce/src/App.tsx
@@ -1,8 +1,6 @@
 import { HashRouter, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar/Navbar'
 import { useTheme } from './hooks/useTheme'
-import { ButtonTest } from './pages/ButtonTest'
-import { ProductTest } from './pages/ProductTest'
 import { Footer } from './components/Footer/Footer'
 import {Route} from "react-router-dom";
 import {ProductDetailPage} from "./pages/ProductDetailPage";
@@ -15,9 +13,8 @@ function App() {
     <HashRouter>
       <Navbar theme={theme} onToggleTheme={toggleTheme} />
       <Routes>
-        <Route path="/test" element={<ButtonTest />} />
-        <Route path="/test" element={<ProductTest />} />
         <Route path="/" element={<CatalogPage />} />
+        <Route path="/catalogo" element={<CatalogPage />} />
         <Route path="/producto/:id" element={<ProductDetailPage />} />
       </Routes>
       <Footer />
